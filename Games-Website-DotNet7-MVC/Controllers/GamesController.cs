c95cb37cdb1647cad77791e1a16e57e0558d67a5
Alobaidy Hussein <96787103+alobaidy7@users.noreply.github.com>
2023-09-29 12:53:31 +0300

    complete create game view, still the post action

---
+++ Games-Website-DotNet7-MVC/Controllers/GamesController.cs
@@ -0,0 +1,35 @@
+﻿using Games_Website_DotNet7_MVC.Data;
+using Games_Website_DotNet7_MVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Games_Website_DotNet7_MVC.Controllers
+{
+    public class GamesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GamesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create() // this is for view the form not posting new game
+        {
+
+            CreateGameFormViewModel viewModel = new()
+            {
+                Categories = _context.Categories.Select(c => new SelectListItem { Value=c.Id.ToString(), Text =c.Name }).OrderBy(c => c.Text).ToList(),
+                Devices = _context.Devices.Select(d => new SelectListItem { Value = d.Id.ToString(), Text = d.Name }).OrderBy(c => c.Text).ToList()
+
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ Games-Website-DotNet7-MVC/ViewModels/CreateGameFormViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Games_Website_DotNet7_MVC.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace Games_Website_DotNet7_MVC.ViewModels
+{
+    public class CreateGameFormViewModel
+    {
+
+        public string Name { get; set; } = string.Empty;
+
+        public IFormFile Cover { get; set; } = default!;
+        
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+        public IEnumerable<SelectListItem> Categories { get; set; } = Enumerable.Empty<SelectListItem>();
+
+        [Display(Name = "Platforms Supported")]
+
+        public List<int> SelectedDevices { get; set; } = new List<int>();
+
+        public IEnumerable<SelectListItem> Devices { get; set; } = Enumerable.Empty<SelectListItem>();
+
+        [MaxLength(2500)]
+        public string Description { get; set; } = string.Empty;
+
+
+    }
+}
